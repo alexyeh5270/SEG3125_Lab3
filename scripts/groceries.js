801383928ce9ed46f942c1012b61156572f5cf46
Alex Seiffert <alexander.c.seiffert@gmail.com>
2026-01-26 14:01:15 -0500

    images + css + product/ cart page logic

--- scripts/groceries.js
+++ scripts/groceries.js
@@ -1,102 +1,109 @@
-	
 // Array of products, each product is an object with different fieldset
-// A set of ingredients should be added to products		 
+// A set of ingredients should be added to products
 
 var products = [
-	{
-		name: "brocoli",
-		vegetarian: true,
-		glutenFree: true,
-		price: 1.99
-	},
-	{
-		name: "bread",
-		vegetarian: true,
-		glutenFree: false,
-		price: 2.35
-	},
-	{
-		name: "salmon",
-		vegetarian: false,
-		glutenFree: true,
-		price: 10.00
-	},
-	{
-		name: "potato",
-		vegetarian: true,
-		glutenFree: true,
-		price: 1.49
-	},
-	{
-		name: "carrot",
-		vegetarian: true,
-		glutenFree: true,
-		price: 1.29
-	},
-	{
-		name: "pasta",
-		vegetarian: true,
-		glutenFree: false,
-		price: 2.99
-	},
-	{
-		name: "celery",
-		vegetarian: true,
-		glutenFree: true,
-		price: 1.99
-	},
-	{
-		name: "chicken",
-		vegetarian: false,
-		glutenFree: true,
-		price: 7.49
-	},
-	{
-		name: "rice",
-		vegetarian: true,
-		glutenFree: true,
-		price: 3.99
-	},
-	{
-		name: "biscuits",
-		vegetarian: true,
-		glutenFree: false,
-		price: 3.49
-	}
-
+  {
+    name: "Brocoli",
+    vegetarian: true,
+    glutenFree: true,
+    price: 1.99,
+    imageUrl: "assets/broccoli.png",
+  },
+  {
+    name: "Bread",
+    vegetarian: true,
+    glutenFree: false,
+    price: 2.35,
+    imageUrl: "assets/bread.png",
+  },
+  {
+    name: "Salmon",
+    vegetarian: false,
+    glutenFree: true,
+    price: 10.0,
+    imageUrl: "assets/salmon.png",
+  },
+  {
+    name: "Potato",
+    vegetarian: true,
+    glutenFree: true,
+    price: 1.49,
+    imageUrl: "assets/potato.png",
+  },
+  {
+    name: "Carrot",
+    vegetarian: true,
+    glutenFree: true,
+    price: 1.29,
+    imageUrl: "assets/carrot.png",
+  },
+  {
+    name: "Pasta",
+    vegetarian: true,
+    glutenFree: false,
+    price: 2.99,
+    imageUrl: "assets/pasta.png",
+  },
+  {
+    name: "Celery",
+    vegetarian: true,
+    glutenFree: true,
+    price: 1.99,
+    imageUrl: "assets/celery.png",
+  },
+  {
+    name: "Chicken",
+    vegetarian: false,
+    glutenFree: true,
+    price: 7.49,
+    imageUrl: "assets/chicken.png",
+  },
+  {
+    name: "Rice",
+    vegetarian: true,
+    glutenFree: true,
+    price: 3.99,
+    imageUrl: "assets/rice.png",
+  },
+  {
+    name: "Biscuits",
+    vegetarian: true,
+    glutenFree: false,
+    price: 3.49,
+    imageUrl: "assets/biscuits.png",
+  },
 ];
-	
-
 
 // given restrictions provided, make a reduced list of products
 // prices should be included in this list, as well as a sort based on price
 
 function restrictListProducts(prods, restriction) {
-	let product_names = [];
-	for (let i=0; i<prods.length; i+=1) {
-		if ((restriction == "Vegetarian&GlutenFree") && (prods[i].vegetarian == true) && (prods[i].glutenFree == true)){
-			product_names.push(prods[i].name);
-		}
-		else if ((restriction == "Vegetarian") && (prods[i].vegetarian == true)){
-			product_names.push(prods[i].name);
-		}
-		else if ((restriction == "GlutenFree") && (prods[i].glutenFree == true)){
-			product_names.push(prods[i].name);
-		}
-		else if (restriction == "None"){
-			product_names.push(prods[i].name);
-		}
-	}
-	return product_names;
+  let product_names = [];
+  for (let i = 0; i < prods.length; i += 1) {
+    if (
+      restriction == "Vegetarian&GlutenFree" &&
+      prods[i].vegetarian == true &&
+      prods[i].glutenFree == true
+    ) {
+      product_names.push(prods[i].name);
+    } else if (restriction == "Vegetarian" && prods[i].vegetarian == true) {
+      product_names.push(prods[i].name);
+    } else if (restriction == "GlutenFree" && prods[i].glutenFree == true) {
+      product_names.push(prods[i].name);
+    } else if (restriction == "None") {
+      product_names.push(prods[i].name);
+    }
+  }
+  return product_names;
 }
 
 // Calculate the total price of items, with received parameter being a list of products
 function getTotalPrice(chosenProducts) {
-	totalPrice = 0;
-	for (let i=0; i<products.length; i+=1) {
-		if (chosenProducts.indexOf(products[i].name) > -1){
-			totalPrice += products[i].price;
-		}
-	}
-	return totalPrice;
+  var totalPrice = 0.0;
+  for (let i = 0; i < products.length; i += 1) {
+    if (chosenProducts.indexOf(products[i].name) > -1) {
+      totalPrice += products[i].price;
+    }
+  }
+  return totalPrice.toFixed(2);
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,94 +1,125 @@
-
 // This function is called when any of the tab is clicked
 // It is adapted from https://www.w3schools.com/howto/howto_js_tabs.asp
 
 function openInfo(evt, tabName) {
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
 
-	// Get all elements with class="tabcontent" and hide them
-	tabcontent = document.getElementsByClassName("tabcontent");
-	for (i = 0; i < tabcontent.length; i++) {
-		tabcontent[i].style.display = "none";
-	}
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
 
-	// Get all elements with class="tablinks" and remove the class "active"
-	tablinks = document.getElementsByClassName("tablinks");
-	for (i = 0; i < tablinks.length; i++) {
-		tablinks[i].className = tablinks[i].className.replace(" active", "");
-	}
-
-	// Show the current tab, and add an "active" class to the button that opened the tab
-	document.getElementById(tabName).style.display = "block";
-	evt.currentTarget.className += " active";
-
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
 }
 
-
-	
 // generate a checkbox list from a list of products
 // it makes each product name as the label for the checkbos
 
 function populateListProductChoices(slct1, slct2) {
-    var s1 = document.getElementById(slct1);
-    var s2 = document.getElementById(slct2);
-	
-	// s2 represents the <div> in the Products tab, which shows the product list, so we first set it empty
-    s2.innerHTML = "";
-		
-	// obtain a reduced list of products based on restrictions
-    var optionArray = restrictListProducts(products, s1.value);
+  var s1 = document.getElementById(slct1);
+  var s2 = document.getElementById(slct2);
 
-	// for each item in the array, create a checkbox element, each containing information such as:
-	// <input type="checkbox" name="product" value="Bread">
-	// <label for="Bread">Bread/label><br>
-		
-	for (i = 0; i < optionArray.length; i++) {
-			
-		var productName = optionArray[i];
-		// create the checkbox and add in HTML DOM
-		var checkbox = document.createElement("input");
-		checkbox.type = "checkbox";
-		checkbox.name = "product";
-		checkbox.value = productName;
-		s2.appendChild(checkbox);
-		
-		// create a label for the checkbox, and also add in HTML DOM
-		var label = document.createElement('label')
-		label.htmlFor = productName;
-		label.appendChild(document.createTextNode(productName));
-		s2.appendChild(label);
-		
-		// create a breakline node and add in HTML DOM
-		s2.appendChild(document.createElement("br"));    
-	}
+  // s2 represents the <div> in the Products tab, which shows the product list, so we first set it empty
+  s2.innerHTML = "";
+
+  // obtain a reduced list of products based on restrictions
+  var optionArray = restrictListProducts(products, s1.value);
+
+  // for each item in the array, create a checkbox element, each containing information such as:
+  // <input type="checkbox" name="product" value="Bread">
+  // <label for="Bread">Bread/label><br>
+
+  for (i = 0; i < optionArray.length; i++) {
+    var productName = optionArray[i];
+    // find the product object to get the image URL
+    var product = products.find((p) => p.name === productName);
+
+    // create a container for the product item
+    var productItem = document.createElement("div");
+    productItem.className = "productItem";
+
+    // create the checkbox and add in HTML DOM
+    var checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.name = "product";
+    checkbox.value = productName;
+    productItem.appendChild(checkbox);
+
+    // create and add image
+    if (product) {
+      var img = document.createElement("img");
+      img.src = product.imageUrl;
+      img.className = "productImage";
+      productItem.appendChild(img);
+    }
+
+    // create a label for the checkbox, and also add in HTML DOM
+    var label = document.createElement("label");
+    label.htmlFor = productName;
+    label.appendChild(
+      document.createTextNode(productName + " - $" + product.price),
+    );
+    productItem.appendChild(label);
+
+    s2.appendChild(productItem);
+  }
 }
-	
+
 // This function is called when the "Add selected items to cart" button in clicked
-// The purpose is to build the HTML to be displayed (a Paragraph) 
+// The purpose is to build the HTML to be displayed (a Paragraph)
 // We build a paragraph to contain the list of selected items, and the total price
 
-function selectedItems(){
-	
-	var ele = document.getElementsByName("product");
-	var chosenProducts = [];
-	
-	var c = document.getElementById('displayCart');
-	c.innerHTML = "";
-	
-	// build list of selected item
-	var para = document.createElement("P");
-	para.innerHTML = "You selected : ";
-	para.appendChild(document.createElement("br"));
-	for (i = 0; i < ele.length; i++) { 
-		if (ele[i].checked) {
-			para.appendChild(document.createTextNode(ele[i].value));
-			para.appendChild(document.createElement("br"));
-			chosenProducts.push(ele[i].value);
-		}
-	}
-		
-	// add paragraph and total price
-	c.appendChild(para);
-	c.appendChild(document.createTextNode("Total Price is " + getTotalPrice(chosenProducts)));
-		
-}
+function selectedItems() {
+  var ele = document.getElementsByName("product");
+  var chosenProducts = [];
 
+  var c = document.getElementById("displayCart");
+  c.innerHTML = "";
+
+  // build list of selected item
+  var para = document.createElement("P");
+  para.innerHTML = "You selected : ";
+  para.appendChild(document.createElement("br"));
+  for (i = 0; i < ele.length; i++) {
+    if (ele[i].checked) {
+      // find the product object to get the image URL
+      var product = products.find((p) => p.name === ele[i].value);
+
+      // create a container for the cart item
+      var cartItem = document.createElement("div");
+      cartItem.className = "cartItem";
+
+      // create and add image
+      if (product) {
+        var img = document.createElement("img");
+        img.src = product.imageUrl;
+        img.className = "cartImage";
+        cartItem.appendChild(img);
+      }
+
+      // add product name
+      cartItem.appendChild(
+        document.createTextNode(ele[i].value + " - $" + product.price),
+      );
+
+      para.appendChild(cartItem);
+      para.appendChild(document.createElement("br"));
+      chosenProducts.push(ele[i].value);
+    }
+  }
+
+  // add paragraph and total price
+  c.appendChild(para);
+  c.appendChild(
+    document.createTextNode(
+      "Total Price is: $" + getTotalPrice(chosenProducts),
+    ),
+  );
+}
